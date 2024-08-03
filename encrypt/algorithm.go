e87dfa0121eaf29f81c306616ddbc4a4553fc2cf
Nevil Jeevan <nevil.jeevan@saint-gobain.com>
2024-08-03 17:28:04 +0530

    add doc

--- encrypt/algorithm.go
+++ encrypt/algorithm.go
@@ -1,5 +1,7 @@
+// This package provides simple text encryption 
 package encrypt
 
+// The Nimbus function encrypts a string by adding 4 to its ascii value
 func Nimbus(input string) (output string) {
 
 	for _, c := range(input) {
